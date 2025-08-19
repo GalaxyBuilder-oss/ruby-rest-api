bb8a4c0fe98eb9919609519f68f557f14c9dbd60
GalaxyBuilder-Oss <galaxybuilder.oss@gmail.com>
2025-08-19 13:32:55 +0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -7,19 +7,19 @@ set :show_exceptions, false
 
 require 'json'
 
-users = [
+$users = [
   { id: 1, name: 'John Doe', email: 'john@example.com' },
   { id: 2, name: 'Jane Doe', email: 'jane@example.com' }
 ]
 
 get '/users' do
   content_type :json
-  users.to_json
+  $users.to_json
 end
 
 get '/users/:id' do
   content_type :json
-  user = users.find { |u| u[:id] == params[:id].to_i }
+  user = $users.find { |u| u[:id] == params[:id].to_i }
   if user
     user.to_json
   else
@@ -31,13 +31,13 @@ post '/users' do
   content_type :json
   new_user = JSON.parse(request.body.read)
   new_user['id'] = users.size + 1
-  users << new_user
+  $users << new_user
   new_user.to_json
 end
 
 put '/users/:id' do
   content_type :json
-  user = users.find { |u| u[:id] == params[:id].to_i }
+  user = $users.find { |u| u[:id] == params[:id].to_i }
   if user
     updated_user = JSON.parse(request.body.read)
     user.merge!(updated_user)
@@ -49,9 +49,9 @@ end
 
 delete '/users/:id' do
   content_type :json
-  user = users.find { |u| u[:id] == params[:id].to_i }
+  user = $users.find { |u| u[:id] == params[:id].to_i }
   if user
-    users.delete(user)
+    $users.delete(user)
     { message: 'User deleted' }.to_json
   else
     halt 404, { message: 'User not found' }.to_json
